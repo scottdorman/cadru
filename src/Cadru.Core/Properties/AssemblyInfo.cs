ea38a747989a49d3ff54914b318518dfe8d3a586
Scott Dorman <scottdorman@outlook.com>
2014-09-17 23:13:57 -0400

    Fixed assembly title attribute.

--- src/Cadru.Core/Properties/AssemblyInfo.cs
+++ src/Cadru.Core/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cadru UnitTest Framework")]
+[assembly: AssemblyTitle("Cadru.Core")]
 [assembly: AssemblyDescription("")]
 
 [assembly: AssemblyFileVersion("1.0.0")]
