40d83e387f7a50295393364fbd286a082017042c
Scott Dorman <scottdorman@outlook.com>
2020-06-27 10:02:39 -0400

    Updated path to the versionupdate targets file.

---
+++ src/Cadru.Caching/CacheKey.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Cadru.Contracts;
+
+namespace Cadru.Caching
+{
+    public class CacheKey
+    {
+        public int Hash { get; }
+
+        public string Key { get; }
+
+        private CacheKey()
+        {
+        }
+
+        public CacheKey(string prefix) : this(prefix, null)
+        {
+            Requires.NotNullOrWhiteSpace(prefix, nameof(prefix));
+        }
+
+        public CacheKey(string prefix, IEnumerable<object> data) : this()
+        {
+            Requires.NotNullOrWhiteSpace(prefix, nameof(prefix));
+
+            var keyBuilder = new List<string>
+                {
+                    prefix
+                };
+
+            if (data != null)
+            {
+                foreach (var d in data)
+                {
+                    if (d != null)
+                    {
+                        if (d is string s)
+                        {
+                            keyBuilder.Add(s);
+                        }
+                        else if (d is IEnumerable e)
+                        {
+                            var tempBuilder = new List<string>();
+
+                            foreach (var element in e)
+                            {
+                                tempBuilder.Add(element.ToString());
+                            }
+
+                            keyBuilder.Add(String.Join("-", tempBuilder));
+                        }
+                        else
+                        {
+                            keyBuilder.Add(d.ToString());
+                        }
+                    }
+                }
+            }
+
+            var cacheKey = String.Join("-", keyBuilder);
+            this.Key = cacheKey;
+            this.Hash = cacheKey.GetHashCode();
+        }
+
+        public override bool Equals(object obj) => (obj is CacheKey key) ? this.Key == key.Key : base.Equals(obj);
+
+        public override int GetHashCode() => this.Hash;
+
+        public override string ToString() => this.Key;
+    }
+}
---
+++ src/Cadru.Caching/CacheKeys.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Cadru.Caching
+{
+    public static partial class CacheKeys
+    {
+        private static readonly HashSet<CacheKey> keys = new HashSet<CacheKey>();
+
+        public static IEnumerable<CacheKey> Keys => keys;
+
+        public static CacheKey CreateKey(string prefix, params object[] data)
+        {
+            var key = new CacheKey(prefix, data);
+            keys.Add(key);
+            return key;
+        }
+
+        public static CacheKey CreateKey(string prefix)
+        {
+            var key = new CacheKey(prefix);
+            keys.Add(key);
+            return key;
+        }
+    }
+}
