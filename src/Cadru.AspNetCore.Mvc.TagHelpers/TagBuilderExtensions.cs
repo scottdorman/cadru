84c384f871cc363f3e2bd14fb847826145386623
Scott Dorman <scottdorman@outlook.com>
2020-09-10 16:25:03 -0400

    Merged extension classes.

--- src/Cadru.AspNetCore.Mvc.TagHelpers/TagBuilderExtensions.cs
+++ src/Cadru.AspNetCore.Mvc.TagHelpers/TagBuilderExtensions.cs
@@ -20,21 +20,165 @@
 // </license>
 //------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using System.Linq;
 
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace Cadru.AspNetCore.Mvc.TagHelpers
 {
+    /// <summary>
+    /// Extension methods for working with <see cref="TagBuilder"/> as a more
+    /// fluent API.
+    /// </summary>
     public static class TagBuilderExtensions
     {
-        public static TagBuilder WithCssClass(this TagBuilder tagBuilder, string value)
+        /// <summary>
+        /// Creates a new HTML tag that has the specified tag name.
+        /// </summary>
+        /// <param name="generator">The <see cref="IHtmlGenerator"/> service.</param>
+        /// <param name="tagName">An HTML tag name.</param>
+        /// <returns>
+        /// A new <see cref="TagBuilder"/> representing the HTML tag.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required for the extension method.")]
+        public static TagBuilder GenerateTag(this IHtmlGenerator generator, string tagName)
         {
-            tagBuilder.AddCssClass(value);
+            var tagBuilder = new TagBuilder(tagName);
             return tagBuilder;
         }
 
-        public static TagBuilder WithCssClasses(this TagBuilder tagBuilder, params string[] values)
+        /// <summary>
+        /// Creates a new HTML tag that has the specified tag name.
+        /// </summary>
+        /// <param name="generator">The <see cref="IHtmlGenerator"/> service.</param>
+        /// <param name="tagName">An HTML tag name.</param>
+        /// <param name="htmlAttributes">
+        /// A dictionary of HTML attributes to merge into the generated tag.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="TagBuilder"/> representing the HTML tag.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required for the extension method.")]
+        public static TagBuilder GenerateTag(this IHtmlGenerator generator, string tagName, IDictionary<string, object> htmlAttributes)
+        {
+            var tagBuilder = new TagBuilder(tagName);
+            tagBuilder.MergeAttributes(htmlAttributes, replaceExisting: true);
+            return tagBuilder;
+        }
+
+        /// <summary>
+        /// Creates a new HTML tag that has the specified tag name.
+        /// </summary>
+        /// <param name="generator">The <see cref="IHtmlGenerator"/> service.</param>
+        /// <param name="tagName">An HTML tag name.</param>
+        /// <param name="htmlAttributes">
+        /// An object of HTML attributes to merge into the generated tag. The
+        /// value can be of type <see cref="IDictionary{TKey,TValue}"/> or
+        /// <see cref="IReadOnlyDictionary{TKey,TValue}"/> or an object with
+        /// public properties as key-value pairs.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="TagBuilder"/> representing the HTML tag.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required for the extension method.")]
+        public static TagBuilder GenerateTag(this IHtmlGenerator generator, string tagName, object htmlAttributes)
+        {
+            return GenerateTag(generator, tagName, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        /// <summary>
+        /// Creates a new HTML tag that has the specified tag name.
+        /// </summary>
+        /// <param name="generator">The <see cref="IHtmlGenerator"/> service.</param>
+        /// <param name="tagName">An HTML tag name.</param>
+        /// <param name="htmlAttributes">
+        /// An object of HTML attributes to merge into the generated tag. The
+        /// value can be of type <see cref="IDictionary{TKey,TValue}"/> or
+        /// <see cref="IReadOnlyDictionary{TKey,TValue}"/> or an object with
+        /// public properties as key-value pairs.
+        /// </param>
+        /// <param name="innerContents">
+        /// One or more <see cref="IHtmlContent"/> objects to be added as inner HTML.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="TagBuilder"/> representing the HTML tag.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required for the extension method.")]
+        public static TagBuilder GenerateTag(this IHtmlGenerator generator, string tagName, object htmlAttributes, params IHtmlContent[] innerContents)
+        {
+            var tagBuilder = new TagBuilder(tagName);
+            tagBuilder.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes), replaceExisting: true);
+            foreach (var innerContent in innerContents ?? Enumerable.Empty<IHtmlContent>())
+            {
+                tagBuilder.InnerHtml.AppendHtml(innerContent);
+            }
+
+            return tagBuilder;
+        }
+
+        /// <summary>
+        /// Creates a new HTML tag that has the specified tag name.
+        /// </summary>
+        /// <param name="generator">The <see cref="IHtmlGenerator"/> service.</param>
+        /// <param name="tagName">An HTML tag name.</param>
+        /// <param name="htmlAttributes">
+        /// An object of HTML attributes to merge into the generated tag. The
+        /// value can be of type <see cref="IDictionary{TKey,TValue}"/> or
+        /// <see cref="IReadOnlyDictionary{TKey,TValue}"/> or an object with
+        /// public properties as key-value pairs.
+        /// </param>
+        /// <param name="innerContent">The HTML inner content to be appended.</param>
+        /// <returns>
+        /// A new <see cref="TagBuilder"/> representing the HTML tag.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required for the extension method.")]
+        public static TagBuilder GenerateTag(this IHtmlGenerator generator, string tagName, object htmlAttributes, string innerContent)
+        {
+            var tagBuilder = new TagBuilder(tagName);
+            tagBuilder.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes), replaceExisting: true);
+            tagBuilder.InnerHtml.AppendHtml(innerContent);
+            return tagBuilder;
+        }
+
+        /// <summary>
+        /// Creates a new HTML tag that has the specified tag name.
+        /// </summary>
+        /// <param name="generator">The <see cref="IHtmlGenerator"/> service.</param>
+        /// <param name="tagName">An HTML tag name.</param>
+        /// <param name="innerContents">
+        /// One or more <see cref="IHtmlContent"/> objects to be added as inner HTML.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="TagBuilder"/> representing the HTML tag.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required for the extension method.")]
+        public static TagBuilder GenerateTag(this IHtmlGenerator generator, string tagName, params IHtmlContent[] innerContents)
+        {
+            var tagBuilder = new TagBuilder(tagName);
+            foreach (var innerContent in innerContents ?? Enumerable.Empty<IHtmlContent>())
+            {
+                tagBuilder.InnerHtml.AppendHtml(innerContent);
+            }
+
+            return tagBuilder;
+        }
+
+        /// <summary>
+        /// Adds a CSS class to the list of CSS classes in the tag. If there are
+        /// already CSS classes on the tag then a space character and the new
+        /// class will be appended to the existing list.
+        /// </summary>
+        /// <param name="tagBuilder">
+        /// The <see cref="TagBuilder"/> instance to update.
+        /// </param>
+        /// <param name="values">The CSS class names to add.</param>
+        /// <returns>
+        /// A new <see cref="TagBuilder"/> representing the HTML tag.
+        /// </returns>
+        public static TagBuilder WithCssClass(this TagBuilder tagBuilder, params string[] values)
         {
             foreach (var value in values ?? Enumerable.Empty<string>())
             {
