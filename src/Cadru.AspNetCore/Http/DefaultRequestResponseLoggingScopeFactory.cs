1d321a85a4fbbe7b107b93d66085b8b03178c14f
Scott Dorman <scottdorman@outlook.com>
2021-03-02 11:04:29 -0500

    Released v5.0.7

--- src/Cadru.AspNetCore/Http/DefaultRequestResponseLoggingScopeFactory.cs
+++ src/Cadru.AspNetCore/Http/DefaultRequestResponseLoggingScopeFactory.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
+using Cadru.Core.Logging;
+
 using Microsoft.AspNetCore.Http;
 
 namespace Cadru.AspNetCore.Http
 {
     /// <summary>
     /// Provides methods for creating a
-    /// <see cref="RequestResponseLoggingScope"/> from an
+    /// <see cref="LoggingScope"/> from an
     /// <see cref="HttpRequestMessage"/> or <see cref="HttpRequest"/>.
     /// </summary>
     public sealed class DefaultRequestResponseLoggingScopeFactory : RequestResponseLoggingScopeFactory
